e368284216c095c649f4e81c7b48f404aadb5347
Erik Svensson <eosvensson@gmail.com>
2021-08-11 11:13:45 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/thymeleafworkshopone/ThymeleafWorkshopOneApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.thymeleafworkshopone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWorkshopOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafWorkshopOneApplication.class, args);
+    }
+
+}
---
+++ src/main/java/se/lexicon/thymeleafworkshopone/controllers/Home.java
@@ -0,0 +1,34 @@
+package se.lexicon.thymeleafworkshopone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class Home {
+
+    private final List<String> strings = new ArrayList<>();
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model){
+        model.addAttribute("strings",strings);
+        return "contact";
+    }
+
+    @PostMapping("/contact/add")
+    public String process(@RequestParam(name = "string") String input){
+        strings.add(input);
+        return "redirect:/contact";
+    }
+
+}
